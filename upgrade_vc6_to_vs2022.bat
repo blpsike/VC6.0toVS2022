76a91f44598b9d1bc34d2df9280f361e456acb0c
blpsike <3257971977@qq.com>
2025-07-16 11:51:11 +0800

    Update upgrade_vc6_to_vs2022.bat

--- upgrade_vc6_to_vs2022.bat
+++ upgrade_vc6_to_vs2022.bat
@@ -1,11 +1,18 @@
-@回显关闭
-正在将VC6项目升级到VS2022...
+@echo off
+setlocal enabledelayedexpansion
 
-rem 设置VS2022开发人员命令提示符环境
-调用 "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
+REM 设置VS2022开发人员命令提示符环境
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
 
-rem 转换项目文件
-devenv.com /upgrade "VC6.0马里奥/新建文件夹/mario01.dsp"
-devenv.com /upgrade "VC6.0马里奥/新建文件夹/mario01.dsw"
+REM 设置GitHub仓库路径
+set repoPath=C:\path\to\your\VC6toVS2022
 
-升级完成。
+REM 遍历仓库内的所有文件夹并尝试升级其中的VC6项目文件
+for /r "%repoPath%" %%i in (*.dsp *.dsw) do (
+    echo 正在升级 %%~fi ...
+    devenv.com /upgrade "%%~fi"
+)
+
+echo 升级完成。
+pause
+
