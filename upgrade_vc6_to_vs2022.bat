2b22b7a6c93f4d218a6dd628a1c7afbf3caa7435
blpsike <3257971977@qq.com>
2025-07-16 17:00:05 +0800

    Update upgrade_vc6_to_vs2022.bat

--- upgrade_vc6_to_vs2022.bat
+++ upgrade_vc6_to_vs2022.bat
@@ -1,18 +1,25 @@
-@echo off
-setlocal enabledelayedexpansion
+@回显关闭
+设置本地环境并启用延迟扩展
+
+REM 设置日志文件路径
+设置日志文件=D:\路径\到\你的\升级日志.txt
 
 REM 设置VS2022开发人员命令提示符环境
-call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
+调用 "D:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat" >> %logFile% 2>&1
 
-REM 设置GitHub仓库路径
-set repoPath=C:\path\to\your\VC6toVS2022
+REM 设置GitHub仓库路径（D盘）
+设置 repoPath=D:\GitHub\VC6toVS2022
 
 REM 遍历仓库内的所有文件夹并尝试升级其中的VC6项目文件
 for /r "%repoPath%" %%i in (*.dsp *.dsw) do (
-    echo 正在升级 %%~fi ...
-    devenv.com /upgrade "%%~fi"
-)
+    echo 正在升级 %%~fi ... >> %logFile%
+    devenv.com /upgrade "%%~fi" >> %logFile% 2>&1
+    如果错误级别为1
+        echo 升级 %%~fi 失败，错误码: %errorlevel% >> %logFile%
+    ) 否则 (
+        echo 升级 %%~fi 成功 >> %logFile%
+    输入：    )
+输入：)
 
-echo 升级完成。
-pause
-
+echo 所有升级操作完成。 >> %logFile%
+暂停
