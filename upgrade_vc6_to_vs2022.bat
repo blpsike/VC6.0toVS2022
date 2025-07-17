286528cd423c4bde3d220c687d5026ca01ce5b85
blpsike <3257971977@qq.com>
2025-07-17 09:27:26 +0800

    Update upgrade_vc6_to_vs2022.bat

--- upgrade_vc6_to_vs2022.bat
+++ upgrade_vc6_to_vs2022.bat
@@ -4,48 +4,14 @@
 REM 设置VS2022开发人员命令提示符环境
 调用 "D:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
 
-REM 设置具体文件路径
-设置 dspFile="D:\gh\VC6.0toVS2022-main\VC6.0马里奥\新建文件夹\mario01.dsp"
-设置 dswFile="D:\gh\VC6.0toVS2022-main\VC6.0马里奥\新建文件夹\mario01.dsw"
+REM 设置GitHub仓库路径
+设置 repoPath="D:\gh\VC6.0toVS2022-main"
 
-REM 初始化日志文件
-设置日志文件=D:\gh\VC6.0toVS2022-main\upgrade_log.txt
-echo 开始升级过程... > "%logFile%"
-
-REM 升级 mario01.dsp
-如果存在 %dspFile% (
-    echo 正在升级 %dspFile% ...
-    echo 正在升级 %dspFile% ... >> "%logFile%"
-    devenv.com /upgrade %dspFile% >> "%logFile%" 2>&1
-    如果错误级别为1
-        echo 升级 %dspFile% 失败，错误码: !errorlevel!
-        echo 升级 %dspFile% 失败，错误码: !errorlevel! >> "%logFile%"
-    ) 否则 (
-        echo 升级 %dspFile% 成功
-        echo 升级 %dspFile% 成功 >> "%logFile%"
-    输入：    )
-) 否则 (
-    echo 文件 %dspFile% 不存在。
-    echo 文件 %dspFile% 不存在。 >> "%logFile%"
+REM 遍历仓库内的所有文件夹并尝试升级其中的VC6项目文件
+for /r "%repoPath%" %%i in (*.dsp *.dsw) do (
+    echo 正在升级 %%~fi ...
+    devenv.com /upgrade "%%~fi"
 输入：)
 
-REM 升级 mario01.dsw
-如果存在 %dswFile% (
-    echo 正在升级 %dswFile% ...
-    echo 正在升级 %dswFile% ... >> "%logFile%"
-    devenv.com /upgrade %dswFile% >> "%logFile%" 2>&1
-    如果错误级别为1
-        echo 升级 %dswFile% 失败，错误码: !errorlevel!
-        echo 升级 %dswFile% 失败，错误码: !errorlevel! >> "%logFile%"
-    ) 否则 (
-        echo 升级 %dswFile% 成功
-        echo 升级 %dswFile% 成功 >> "%logFile%"
-    输入：    )
-) 否则 (
-    echo 文件 %dswFile% 不存在。
-    echo 文件 %dswFile% 不存在。 >> "%logFile%"
-输入：)
-
-所有升级操作完成。
-echo 所有升级操作完成。 >> "%logFile%"
+echo 升级完成。
 暂停
